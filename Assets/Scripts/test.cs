48a08f9c5fe6580ce0833083a65cbee2efdcbc76
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-09-28 02:11:37 +0900

    とりあえず使いそうなものを追加しました

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Saito.Audio;
+using UniRx;
+
+public class test:MonoBehaviour
+    {
+        [SerializeField] private AudioClip _clip;
+        [SerializeField] private float _volume;
+        
+        private void Start()
+        {
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetKeyDown(KeyCode.A))
+                .Subscribe(_ =>
+                {
+                    AudioManager.Instance.SetDelay(0.0f)
+                        .SetPitch(1.0f)
+                        .SetVolume(_volume)
+                        .PlaySE(_clip, 1)
+                        .OnComplete(() => Debug.Log("Complete!"));
+                }).AddTo(this);
+        }
+    }
---
+++ Assets/Scripts/unirxtest.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class unirxtest : MonoBehaviour
+{
+    public IObservable<unirxtest> Play()
+    {
+        return null;
+    }
+    
+    public IObservable<unirxtest> SetDelay()
+    {
+        return null;
+    }
+}
